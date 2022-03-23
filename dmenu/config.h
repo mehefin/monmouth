31c008fb62daeda52d75a3411e8c5342d3d9e9fe
mehefin <cryptomorphinized@protonmail.com>
2022-03-24 02:23:34 +0300

    ---

---
+++ dmenu/config.h
@@ -0,0 +1,23 @@
+/* See LICENSE file for copyright and license details. */
+/* Default settings; can be overriden by command line. */
+
+static int topbar = 0;                      /* -b  option; if 0, dmenu appears at bottom     */
+/* -fn option overrides fonts[0]; default X11 font or font set */
+static const char *fonts[] = {
+	"Fuzzy Bubbles:style=Bold:pixelsize=15:antialias=true"
+};
+static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#cccccc", "#090909" },
+	[SchemeSel] = { "#090909", "#cccccc" },
+	[SchemeOut] = { "#090909", "#cccccc" },
+};
+/* -l option; if nonzero, dmenu uses vertical list with given number of lines */
+static unsigned int lines      = 0;
+
+/*
+ * Characters not considered part of a word while deleting words
+ * for example: " /?\"&[]"
+ */
+static const char worddelimiters[] = " ";
